154eed6de95259a755f47c8dbce196efdc2d60ca
nadiradad <nadiradad@gmail.com>
2024-11-14 21:11:33 -0300

    comentario

---
+++ modelo_orm.py
@@ -0,0 +1,5 @@
+"""Crear el módulo “modelo_orm.py” que contenga la definición de las clases y atributos que
+considere necesarios, siguiendo el modelo ORM de Peewee para poder persistir los datos
+importados del dataset en una base de datos relacional de tipo SQLite llamada
+“obras_urbanas.db”, ubicada en la misma carpeta solución del proyecto. Aquí se debe incluir
+además la clase BaseModel heredando de peewee.Model."""
