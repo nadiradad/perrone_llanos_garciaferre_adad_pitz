e623a74d82b27d6660ab9e5a67267ef26858cf57
nadiradad <nadiradad@gmail.com>
2024-11-18 21:39:47 -0300

    hice incrementar_plazo e iniciar_contratacion

--- incrementar_plazo.py
+++ incrementar_plazo.py
@@ -1,10 +1,23 @@
 def incrementar_plazo():
-    nuevo_plazo = input('Ingrese el nuevo plazo en cantidad de meses: ')
+    try:
+        nuevo_plazo = int(input('Ingrese el nuevo plazo en cantidad de meses: '))
+        if nuevo_plazo <= 0:
+            print("El plazo debe ser un número positivo.")
+            return None
+    except ValueError:
+        print("Debe ingresar un número entero válido.")
+        return None
+
     nombre_obra = input('Ingrese el nombre de la obra para incrementar el plazo: ')
     try:
-        obra = Obra.get(Obra.nombre == nombre_obra)
-        obra.plazo_meses = nuevo_plazo      # Asigna el nuevo valor a plazo_meses
-        obra.save()                         # Guarda los cambios en la base de datos
-        print(f"El plazo de la obra {nombre_obra} ha sido actualizado a {nuevo_plazo} meses.")
-    except Obra.DoesNotExist:
-        print("No se encontró la obra con ese nombre.")
+        obra = Obra.get_or_none(Obra.nombre == nombre_obra)
+        if not obra:
+            print("No existe la obra deseada.")
+            return None
+        
+        obra.plazo_meses = nuevo_plazo
+        obra.save()
+        print(f"El plazo de la obra '{nombre_obra}' ha sido actualizado a {nuevo_plazo} meses.")
+    except Exception as e:
+        print(f"No se pudo modificar el plazo. Error: {e}")
+
